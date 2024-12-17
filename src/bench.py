57f62bbf1adb075cc223f9944ed3a25c32d7894e
Lucas Oliveira Maggi <lucas.maggi@usp.br>
2024-12-17 11:40:32 -0300

    [ADDED] csv writing, iterative runs.

--- src/bench.py
+++ src/bench.py
@@ -1,6 +1,7 @@
 from bencher import Bencher, BencherConfig
 import matplotlib.pyplot as plt
 import numpy as np
+import os, sys
 
 def callback(b: Bencher):
     # print(f"b.cpu_pct: {b.cpu_pct[-1]} % | b.mem_info: {b.mem_info[-1]} | b.read_bytes:{b.read_bytes[-1]}")
@@ -17,9 +18,13 @@ def main():
     # cfg.executable = "firefox"
     cfg.name = "gedit"
     cfg.executable = "gedit"
-    cfg.args = []
+    cfg.args = [""]
     cfg.callback = callback
     cfg.sample_time = 1
+    cfg.sample_total = 3
+    
+    # Repeat the evaluaton for n times
+    repeat_n_times = 3
     
     # Creating Bencher
     b = Bencher()
@@ -28,15 +33,24 @@ def main():
     b.set(cfg)
     
     # Running Bencher
-    b.run()
+    for i in range(repeat_n_times):
+        b.run()
     
-    plt.plot(b.sample_timestamp, b.cpu_pct)
+    # Save data
+    os.makedirs(f'results/{b.name}/', exist_ok=True)
+    for i in range(repeat_n_times):
+        df = b.data_history[i].to_data_frame()
+        df.to_csv(f'results/{b.name}/{i}.csv')
+    
+    for i in range(repeat_n_times):
+        plt.plot(b.data_history[i].sample_timestamp, b.data_history[i].cpu_pct)
     plt.title(f"{b.name}'s CPU Usage (%)")
     plt.ylabel("CPU Usage (%)")
     plt.xlabel("Sample timestamp (s)")
     plt.show()
     
-    plt.plot(b.sample_timestamp, [x.data for x in b.mem_info])
+    for i in range(repeat_n_times):
+        plt.plot(b.data_history[i].sample_timestamp, [x for x in b.data_history[i].mem_info])
     plt.title(f"{b.name}'s RAM Usage (bytes)")
     plt.ylabel("RAM Usage (bytes)")
     plt.xlabel("Sample timestamp (s)")
--- src/bencher.py
+++ src/bencher.py
@@ -1,6 +1,7 @@
 import psutil
 import subprocess
 import time
+import pandas
 
 
 class BencherConfig:
@@ -10,13 +11,10 @@ class BencherConfig:
         self.args = list([])
         self.callback = None
         self.sample_time = 1
-        
+        self.sample_total = 100
 
-class Bencher:
-    
+class BencherData:
     def __init__(self):
-        
-        # Data
         self.cpu_pct = list()
         self.mem_info = list()
         self.num_threads = list()
@@ -25,28 +23,9 @@ class Bencher:
         self.write_count = list()
         self.write_bytes = list()
         self.sample_timestamp = list()
-        
-        # Config
-        self.name = "unnamed"
-        self.executable = ""
-        self.args = list([""])
-        self.callback = None
-        self.sample_time = 1
-        
-        # Status
-        self.done = False
-        self.init_time = 0
+        self.process_died = None
     
-    def set(self, config: BencherConfig):
-        self.name = config.name
-        self.executable = config.executable
-        self.args = config.args
-        self.callback = config.callback
-        self.sample_time = config.sample_time
-    
-    def reset(self):
-        
-        # Data
+    def clear(self):
         self.cpu_pct.clear()
         self.mem_info.clear()
         self.num_threads.clear()
@@ -55,11 +34,74 @@ class Bencher:
         self.write_count.clear()
         self.write_bytes.clear()
         self.sample_timestamp.clear()
+        self.process_died = None
+    
+    def to_dict(self) -> dict:
+        d = dict()
+        d['cpu_pct'] = self.cpu_pct
+        d['mem_info'] = self.mem_info
+        d['num_threads'] = self.num_threads
+        d['read_count'] = self.read_count
+        d['read_bytes'] = self.read_bytes
+        d['write_count'] = self.write_count
+        d['write_bytes'] = self.write_bytes
+        d['sample_timestamp'] = self.sample_timestamp
+        
+        for k in d.keys():
+            print(f'{k}:{len(d[k])}')
+        print("-----------------------------")
+            
+        return d
+        
+    def to_data_frame(self) -> pandas.DataFrame:
+        df = pandas.DataFrame()
+        df = df.from_dict(self.to_dict())
+        return df
+
+class Bencher:
+    
+    def __init__(self):
+        
+        # Data
+        self.data = None
+        self.data_history = list([])
+        
+        # Config
+        self.name = "unnamed"
+        self.executable = ""
+        self.args = list([""])
+        self.callback = None
+        self.sample_time = 1
+        self.sample_total = 100
+        self.sample_current = 0
+        
+        # Status
+        self.done = False
+        self.init_time = 0
+        self.process_died = False
+        
+    
+    def set(self, config: BencherConfig):
+        self.name = config.name
+        self.executable = config.executable
+        self.args = config.args
+        self.callback = config.callback
+        self.sample_time = config.sample_time
+        self.sample_total = config.sample_total
+        self.sample_current = 0
+        
+    def run(self):
+        
+        # Data
+        self.data = BencherData()
+        
+        # Config
+        self.sample_current = 0
         
         # Status
         self.done = False
         
-    def run(self):
+        # Run Loop
         try:
             if not self.done:
                 self.init_time = time.time()
@@ -68,29 +110,52 @@ class Bencher:
                 cpu_count = psutil.cpu_count()
                 
                 while self.p.is_running() and not self.done:
-                    self.sample_timestamp.append(time.time() - self.init_time)
+                    self.data.sample_timestamp.append(time.time() - self.init_time)
+                    
                     # CPU metrics
-                    self.cpu_pct.append(self.p.cpu_percent()/cpu_count)
-                    self.num_threads.append(self.p.num_threads())
+                    self.data.cpu_pct.append(self.p.cpu_percent()/cpu_count)
+                    self.data.num_threads.append(self.p.num_threads())
                     
                     # Memory Metrics
-                    self.mem_info.append(self.p.memory_info())
+                    self.data.mem_info.append(self.p.memory_info().data)
                     
                     # IO metrics
-                    io_counters = self.p.io_counters() # read count | write count | read bytes | write bytes
-                    self.read_count.append(io_counters.read_count)
-                    self.read_bytes.append(io_counters.read_bytes)
-                    self.write_count.append(io_counters.write_count)
-                    self.write_bytes.append(io_counters.write_bytes)
+                    try:
+                        io_counters = self.p.io_counters() # read count | write count | read bytes | write bytes
+                        print(f'io_counters:{io_counters}')
+                    except psutil.AccessDenied as e:
+                        self.process_died = True
+                        print("Access Denied")
+                        self.data.read_count.append(0)
+                        self.data.read_bytes.append(0)
+                        self.data.write_count.append(0)
+                        self.data.write_bytes.append(0)
+                    finally:
+                        self.data.read_count.append(io_counters.read_count)
+                        self.data.read_bytes.append(io_counters.read_bytes)
+                        self.data.write_count.append(io_counters.write_count)
+                        self.data.write_bytes.append(io_counters.write_bytes)
                     
                     if self.callback is not None:
                         self.callback(self)
+                    
+                    self.sample_current = self.sample_current + 1
+                    
+                    if self.sample_current == self.sample_total:
+                        self.p.kill()
+                        break
+                    
                     time.sleep(self.sample_time)
                 
-                self.done = True
             else:
                 print(f"The bencher for '{self.executable}' with {self.args} is done already. Doing nothing.")
         except KeyboardInterrupt as e:
             self.p.kill()
+            raise KeyboardInterrupt()
         except psutil.AccessDenied as e:
-            pass
+            self.process_died = True
+            print("Access Denied")
+        finally:
+            self.data.process_died = self.process_died
+            self.data_history.append(self.data)
+            self.done = True
